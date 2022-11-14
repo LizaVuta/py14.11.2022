2052045acc8bf9e05e6194b1df4fb9e69a451c6b
Liza Vuta <vita.rita.00@mail.ru>
2022-11-14 17:17:58 +0300

    version 4

--- 14.11.2022.py
+++ 14.11.2022.py
@@ -6,8 +6,13 @@ df= pd.read_csv('C:/Users/digdepstudent/Documents/Вута/14.11.2022/py14.11.20
 #rint(df.to_string())
 print(df.info())
 df=df.dropna(axis=0)
-print(df.to_string())
+
 x= df['age'].mean()
 df['age'].fillna(x, inplace= True)
 print(df.to_string())
 print(df.info())
+df.drop_duplicates(inplace= True)
+print(df.to_string())
+print(df.info())
+df['sex_num']=0.0
+print(df.to_string())
